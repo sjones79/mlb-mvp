7ad77c526b5d9a809d3e0b25f4d2df1162236cde
sjones79 <jonessm@umich.edu>
2016-01-02 15:49:14 -0500

    Show and hide gauge charts based on menu selection

--- scripts/chartUtils.js
+++ scripts/chartUtils.js
@@ -1,4 +1,66 @@
-var showHideChart = function() {
-    $('#pitchTypeTotalLineChart').show();
+var showPitchVelocityMenu = function() {
+    $("#pitchMenuSection").removeClass("notVisible").addClass("visible");
+}
 
+var showHideGauges = function(option){
+    $('.gauge').hide();
+    
+    var selectedOption = option.data('value');
+    
+    switch(selectedOption){
+        case 'ch':
+            $('#pitchSpeedGaugeCH').show();
+            $('#spinRotationGaugeCH').show();
+        break;
+        
+        case 'cu':
+            $('#pitchSpeedGaugeCU').show();
+            $('#spinRotationGaugeCU').show();
+        break;
+        
+        case 'fc':
+            $('#pitchSpeedGaugeFC').show();
+            $('#spinRotationGaugeFC').show();
+        break;
+        
+        case 'ff':
+            $('#pitchSpeedGaugeFF').show();
+            $('#spinRotationGaugeFF').show();
+        break;
+        
+        case 'fs':
+            $('#pitchSpeedGaugeFS').show();
+            $('#spinRotationGaugeFS').show();
+        break;
+        
+        case 'ft':
+            $('#pitchSpeedGaugeFT').show();
+            $('#spinRotationGaugeFT').show();
+        break;
+        
+        case 'in':
+            $('#pitchSpeedGaugeIN').show();
+            $('#spinRotationGaugeIN').show();
+        break;
+        
+        case 'kc':
+            $('#pitchSpeedGaugeKC').show();
+            $('#spinRotationGaugeKC').show();
+        break;
+        
+        case 'si':
+            $('#pitchSpeedGaugeSI').show();
+            $('#spinRotationGaugeSI').show();
+        break;
+        
+        case 'sl':
+            $('#pitchSpeedGaugeSL').show();
+            $('#spinRotationGaugeSL').show();
+        break;
+            
+
+    }
+    
+            
+                                                
 }
