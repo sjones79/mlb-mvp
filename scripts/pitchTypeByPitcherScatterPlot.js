b37aa887b5806a27b4e0e7beb2a867a8c20a17e8
sjones79 <jonessm@umich.edu>
2016-01-02 22:57:52 -0500

    Update the text on the chart

--- scripts/pitchTypeByPitcherScatterPlot.js
+++ scripts/pitchTypeByPitcherScatterPlot.js
@@ -1,6 +1,7 @@
 var pitchTypeByPitcher = function(pitcherMap, seriesData) {
     
     var pitcherNames = Object.keys(pitcherMap);
+    console.log("pitcher names in the chart", pitcherMap);
         
     var pitchTypeChart = new Highcharts.Chart({
         chart: {
@@ -13,36 +14,35 @@ var pitchTypeByPitcher = function(pitcherMap, seriesData) {
             x: -20 //center
         },
         subtitle: {
-            text: 'Click Pitcher Name Below to Filter Data',
+            text: 'Click Pitch Type Below to Filter Data',
             x: -20
         },
         xAxis: {
             title: {
                 enabled: true,
-                text: 'Pitch Types'
-            }
+                text: 'Pitchers'
+            },
+            categories: pitcherNames
             
         },
         tooltip: {
             headerFormat: '<b>{series.name}</b><br>',
             formatter:function(){
-                return '<strong>' + this.series.name + '</strong>' + ' threw ' + '<strong>' + this.key + '</strong> ' + this.y + ' times.';
+                return '<strong>' + this.key  + '</strong>' + ' threw ' + '<strong>' + this.series.name  + '</strong> ' + this.y + ' times.';
             }
                     
         },
         yAxis: {
             title: {
                 text: '# Pitches Per Type'
-            },
-            
-            categories: pitcherNames
+            }
 
         },
         
         plotOptions: {
             scatter: {
                 marker: {
-                    radius: 5,
+                    radius: 4,
                     states: {
                         hover: {
                             enabled: true,
